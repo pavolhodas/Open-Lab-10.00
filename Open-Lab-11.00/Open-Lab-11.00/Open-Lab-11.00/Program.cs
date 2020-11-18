18459d67cfa971d994fd674736991402e722b1ba
pavolhodas <71751188+pavolhodas@users.noreply.github.com>
2020-11-18 19:20:20 +0100

    Add files via upload

---
+++ Open-Lab-11.00/Open-Lab-11.00/Open-Lab-11.00/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Open_Lab_11._00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Library schoolLibrary = new Library();
+            schoolLibrary.NumberOfBooks = 4;
+            schoolLibrary.NumberOfStudents = 2;
+
+            schoolLibrary.StudentsBook[0] = 2;
+            schoolLibrary.StudentsBook[1] = 2;
+            schoolLibrary.StudentsList[0] = "Paul";
+            schoolLibrary.StudentsList[1] = "John ";
+
+            schoolLibrary.WriteAll();
+        }
+    }
+}
